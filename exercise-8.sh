2b9e2417a8d9fc0c109842fb07d5f1a09fa17e13
Bharath <bharathdcs@gmail.com>
2024-11-28 13:16:40 +1100

    Update exercise-8.sh

--- exercise-8.sh
+++ exercise-8.sh
@@ -1,3 +1,10 @@
+echo "Install AWS Cli"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+sleep 20
+
 oc new-project test
 
 oc apply -f - <<EOF
